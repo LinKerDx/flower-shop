faf4464256322558fb64b7bc0a40cd0d5a7826a0
LinKDx <omar_150596@hotmail.com>
2025-09-12 14:14:17 -0600

    Update UI spacing and text semantics in components
    Changed a heading to a paragraph in NoEncuentraProductos for better semantic accuracy and text alignment. Adjusted top margin in Hero section from mt-20 to mt-16 to refine layout spacing.

--- src/app/components/NoEncuentraProductos.tsx
+++ src/app/components/NoEncuentraProductos.tsx
@@ -1,7 +1,7 @@
 export default function NoEncuentraProductos() {
     return (
         <div className="flex flex-col items-center justify-center mt-20 gap-10 col-span-full h-120">
-            <h2 className="text-2xl font-bold">No hay productos que coincidan con los filtros seleccionados.</h2>
+            <p className="text-2xl font-bold text-center">No hay productos que coincidan con los filtros seleccionados.</p>
             <p className="text-lg">Intenta ajustar los filtros o restablecerlos.</p>
         </div>
 
